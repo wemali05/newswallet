a11739b8f4728655339c01f5b77517be1bdacec3
wemali05 <alfredwemali05@gmail.com>
2018-11-09 03:39:08 +0300

    Add articles model and migration

---
+++ database/migrations/2018_11_09_003132_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->references('categories')->on('id');
+            $table->string('title');
+            $table->string('image_path')->nullable();
+            $table->string('author_name')->nullable();
+            $table->string('website_name');
+            $table->string('link');
+            $table->mediumText('description')->nullable();
+            $table->integer('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
