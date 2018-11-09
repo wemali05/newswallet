2ee74dd2f3735dd531a64f3d0aada3a158eb74c0
wemali05 <alfredwemali05@gmail.com>
2018-11-09 04:21:41 +0300

    Add articles api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'v0'
+], function() {
+    Route::get('articles', 'Api\ArticleController@index');
+    Route::get('articles/{article}', 'Api\ArticleController@show');
+    Route::post('articles', 'Api\ArticleController@store');
+    Route::put('articles/{article}', 'Api\ArticleController@update');
+    Route::delete('articles/{article}', 'Api\ArticleController@delete');
+});
