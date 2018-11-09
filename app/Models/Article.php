eba6af54f2ea6beea18cdc1166e04054b728634a
wemali05 <alfredwemali05@gmail.com>
2018-11-09 03:47:50 +0300

    Specify mass assignable fields on articles table

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id', 
+        'title', 
+        'image_path',
+        'author_name',
+        'website_name',
+        'link',
+        'description'
+    ];
 }
