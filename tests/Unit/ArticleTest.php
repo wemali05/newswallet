5c1aa3d1efeb5840eba86e65bcc7f78ed4709cc6
wemali05 <alfredwemali05@gmail.com>
2018-11-10 06:00:24 +0300

    Add method to test all articles are returned

--- tests/Unit/ArticleTest.php
+++ tests/Unit/ArticleTest.php
@@ -18,6 +18,9 @@ class ArticleTest extends TestCase
         $this->assertTrue(true);
     }
 
+    /**
+     * Test if an article can be created outside the middleware
+     */
     public function testCreateArticleWithMiddleware()
     {
         $data = [
@@ -34,5 +37,41 @@ class ArticleTest extends TestCase
         $response = $this->json('POST', '/api/v0/articles',$data);
         $response->assertStatus(401);
         $response->assertJson(['message' => "Unauthenticated."]);
+    }
+
+    /**
+     * Test if an article was created
+     */
+    public function testCreateArticle()
+    {
+        $data = [
+            'category_id' => 1,
+            'title' => "This is an article",
+            'image_path' => "https://images.pexels.com/photos/1000084/pexels-photo-1000084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            'author_name' => 'KuzaLab',
+            'website_name' => "kuzalab.com",
+            "link" => "http://kuzalab.com",
+            "description" => "This is the description"
+        ];
+        $user = factory(\App\Models\User::class)->create();
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/v0/articles',$data);
+        $response->assertStatus(200);
+        $response->assertJson(['status' => true]);
+        $response->assertJson(['data' => $data]);
+    }
+
+    /**
+     * Test getting all articles
+     */
+    public function testGettingAllProducts()
+    {
+        $response = $this->json('GET', '/api/v0/articles');
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure(
+            [
+                ['id', 'category_id', 'title', 'image_path', 'author_name', 'website_name', 'link', 'description', 'views', 'created_at', 'updated_at']
+            ]
+        );
     }
 }
